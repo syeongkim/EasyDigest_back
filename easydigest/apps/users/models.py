81815ceb86e4a858d980bab278a16119986b1df0
syeongkim <carsk0607@gmail.com>
2025-04-29 16:51:58 +0900

    add signup/login/logout api

--- easydigest/apps/users/models.py
+++ easydigest/apps/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    class InterestChoices(models.TextChoices):
+        POLITICS = "정치", "정치"
+        SOCIAL = "사회", "사회"
+        ECONOMY = "경제", "경제"
+        CULTURE = "문화", "문화"
+        TECHNOLOGY = "기술", "기술"
+
+    interest = models.CharField(
+        max_length=20,
+        choices=InterestChoices.choices,
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
