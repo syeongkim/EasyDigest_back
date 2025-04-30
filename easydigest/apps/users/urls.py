47331e8b3b133104774a553f8747819d6cd7ce15
syeongkim <carsk0607@gmail.com>
2025-05-01 01:16:29 +0900

    fix user urls.py

--- easydigest/apps/users/urls.py
+++ easydigest/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import signup, login_view, logout_view
+from .views import signup, login_view, logout_view, check_username
 
 urlpatterns = [
     path('signup/', signup),
     path('login/', login_view),
     path('logout/', logout_view),
+    path('check-username/', check_username),
 ]
