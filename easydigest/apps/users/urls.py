625ece77b37a7a085283acdc985de66897ab35db
syeongkim <carsk0607@gmail.com>
2025-05-13 20:09:46 +0900

    add mypage related api

--- easydigest/apps/users/urls.py
+++ easydigest/apps/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import signup, login_view, logout_view, check_username
+from .views import signup, login_view, logout_view, check_username, get_profile, update_profile, change_password
 
 urlpatterns = [
     path('signup/', signup),
     path('login/', login_view),
     path('logout/', logout_view),
     path('check-username/', check_username),
+    path('me/', get_profile),
+    path('me/update/', update_profile),
+    path('change-password/', change_password)
 ]
