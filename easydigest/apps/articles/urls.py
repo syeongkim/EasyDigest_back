08e9bb3da7fb79d7dde1e9137878e3453d803ba1
syeongkim <carsk0607@gmail.com>
2025-05-16 01:13:06 +0900

    feat: add article summary api

--- easydigest/apps/articles/urls.py
+++ easydigest/apps/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail, my_articles
+from .views import article_list, article_detail, my_articles, generate_summary
 
 urlpatterns = [
     path('', article_list),
     path('<int:article_id>/', article_detail),
     path('my/', my_articles),
+    path('<int:article_id>/generate-summary/', generate_summary)
 ]
