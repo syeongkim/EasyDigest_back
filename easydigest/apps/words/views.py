a5c10412fcabf4db07223115734c9138a34bd6f3
syeongkim <carsk0607@gmail.com>
2025-05-19 15:13:38 +0900

    gpt로 자연스러운 설명 생성 추가

--- easydigest/apps/words/views.py
+++ easydigest/apps/words/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from .models import Word, LearnedWord
 from .serializers import WordSerializer
 from django.utils import timezone
-from .gpt import analyze_pos_with_stanza, infer_overall_pos, retrieve_definition, generate_definition_with_gpt
+from .gpt import analyze_pos_with_stanza, infer_overall_pos, retrieve_definition, generate_definition_with_gpt, simplify_with_gpt
 from apps.articles.models import Article
 import re, random
 
@@ -43,7 +43,8 @@ def learn_word(request):
     # description = explain_word_in_context(article.content, word_text)
     description = retrieve_definition(word_text)
     if not description:
-        description = generate_definition_with_gpt(word_text)
+        description = generate_definition_with_gpt(description)
+    simplify_with_gpt(description, word_text)
 
     # 2. pos 분류 (추가 필요)
     toks = analyze_pos_with_stanza(word_text)
